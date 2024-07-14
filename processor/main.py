5d9b035b34522764b43f7b2fb8c81781452c61e0
Carlos Palma <chonwil@gmail.com>
2024-07-14 19:08:23 -0300

    Initialize database for processing

--- processor/main.py
+++ processor/main.py
@@ -10,7 +10,6 @@ parent_dir = os.path.dirname(current_dir)
 shared_dir = os.path.join(parent_dir, "shared")
 sys.path.append(parent_dir)
 
-from processor import Processor, ProcessorResult
 
 def initiate_logs(log_level = "INFO"):
     # Configure loguru
@@ -19,6 +18,28 @@ def initiate_logs(log_level = "INFO"):
     logger.remove()  # Remove default handler
     logger.add(sys.stderr, level=log_level)
     logger.add(log_file_name, rotation="10 MB", level=log_level)
+    
+def initialize_db():
+    # Clear the content of all the database tables, EXCEPT for the Scraper tables
+    from shared.utils import DBHelper
+    db = DBHelper()
+    db.execute_query("""TRUNCATE
+                        car_articles, 
+                        similar_cars, 
+                        similar_launches, 
+                        article_sections, 
+                        articles, 
+                        unclassified_car_sales, 
+                        similar_launches, 
+                        cars,
+                        launches,
+                        car_sales,
+                        car_models,
+                        car_prices,
+                        sales_reports
+                        RESTART IDENTITY""")
+    db.execute_query("UPDATE posts SET date_parsed = NULL")
+    logger.info("Database tables cleared.")
 
 def main():
     parser = argparse.ArgumentParser(description="Data processor for posts scraped from https://www.autoblog.com.uy")
@@ -42,6 +63,12 @@ def main():
         default="INFO",
         help="Set the logging level"
     )
+    parser.add_argument(
+        "--init-db",
+        action="store_true",
+        help="Initialize the database by clearing all tables"
+    )
+    
     
     args = parser.parse_args()
     
@@ -49,6 +76,16 @@ def main():
     initiate_logs(args.log_level)
 
     logger.info(f"Starting processor with options: {args.options}, actions: {args.actions}")
+    
+    if args.init_db:
+        confirm = input("Are you sure you want to initialize the database? This will clear all the tables and reset the posts dates. It will NOT clear any scraped pages. Type 'yes' to confirm: ")
+        if confirm.lower() == "yes":
+            initialize_db()
+        else:
+            logger.info("Database initialization canceled by the user.")
+        return
+
+    from processor import Processor, ProcessorResult
     
     processor = Processor()
     result = processor.process(actions=args.actions, entities=args.options)
